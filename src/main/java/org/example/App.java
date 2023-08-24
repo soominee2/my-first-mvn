8222959778087ea266ee99f5acac87d0b2006cdb
Soomin Han <mmovie00@daum.net>
2023-08-24 16:45:30 +0900

    계산기 코드 추가, 테스트 코드 추가

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,16 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        //계산기 사용 덧셈 //
+//        Calculator calc = new Calculator();
+//
+//        int result = calc.add(5, 10);
+//        System.out.println("Result=" + result);
     }
 }
